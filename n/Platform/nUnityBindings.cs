066a42ce8e96a154384d4589b67fa424641cc72d
Doug <linderd@iinet.net.au>
2012-11-02 16:47:55 +0800

    clean and added dummy db impls

--- n/Platform/nUnityBindings.cs
+++ n/Platform/nUnityBindings.cs
@@ -1,14 +1,19 @@
 using System;
+using n.Platform.Db;
 
 namespace n.Platform
 {
-  public class nUnityBindings
+  public class nUnityBinding
 	{
     public static void Bind(nResolver resolver) {
       resolver.Bind<nContext, nUnityContext>();
-      resolver.Bind<nDb, nUnityDb>();
       resolver.Bind<nDispatcher, nUnityDispatcher>();
       resolver.Bind<nLogWriter, nUnityLogWriter>();
+      #if UNITY_WEBPLAYER
+        resolver.Bind<nDb, CookieRepo>();
+      #else
+        resolver.Bind<nDb, SqliteRepo>();
+      #endif
     }
 	}
 }
